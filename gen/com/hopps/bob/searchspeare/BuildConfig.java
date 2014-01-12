de21fbc5aa0027529aa0b0046edf3cb22c45482f
kye308 <kevincye1996@gmail.com>
2014-01-11 17:07:11 -0800

    Loaded files to github

---
+++ gen/com/hopps/bob/searchspeare/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.hopps.bob.searchspeare;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/hopps/bob/searchspeare/QuoteActivity.java
@@ -0,0 +1,82 @@
+package com.hopps.bob.searchspeare;
+
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+/* Displays quotes and their locations within a play.
+ * 
+ */
+public class QuoteActivity extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quote);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+            ActionBar actionBar = getActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        Uri uri = getIntent().getData();
+        Cursor cursor = managedQuery(uri, null, null, null, null);
+
+        if (cursor == null) {
+            finish();
+        } else {
+            cursor.moveToFirst();
+
+            TextView quote = (TextView) findViewById(R.id.quote);
+            TextView location = (TextView) findViewById(R.id.location);
+
+            int lIndex = cursor.getColumnIndexOrThrow(QuoteDatabase.KEY_LOCATION);
+            int qIndex = cursor.getColumnIndexOrThrow(QuoteDatabase.KEY_QUOTE);
+
+            quote.setText(cursor.getString(qIndex));
+            location.setText(cursor.getString(lIndex));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+            SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+            SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            searchView.setIconifiedByDefault(false);
+        }
+        
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search:
+                onSearchRequested();
+                return true;
+            case android.R.id.home:
+                Intent intent = new Intent(this, Searchspeare.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return false;
+        }
+    }
+}
